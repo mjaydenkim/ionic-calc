d7cf0da51432fbc9bfa5b3eca9f4b8e81a3497a2
SMTPlaceholderUsername <70102310+SMTPlaceholderUsername@users.noreply.github.com>
2022-09-22 19:08:26 -0700

    Added room component

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import storeMethods from '../../models/Room/store'
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
@@ -10,5 +10,17 @@ export class RoomComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {}
+
+  name: string = ""
+
+  updateName(event: any) {
+    console.log(event)
+    this.name = event.target.value
+  }
+
+  handleSubmit(event: any) {
+    console.log("Creating a room with name: " + this.name)
+    event.preventDefault() 
+  }
 
 }
