e4f0fd4f7a402d2ab7ffd83b82fcd8bebfc0c757
SMTPlaceholderUsername <70102310+SMTPlaceholderUsername@users.noreply.github.com>
2022-09-22 19:47:20 -0700

    Added room generation from frontend

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import storeMethods from '../../models/Room/store'
+
+import { API, graphqlOperation } from 'aws-amplify'
+import { createRoom } from 'src/graphql/mutations';
+
+import { v4  as uuid } from 'uuid'
+
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
@@ -18,9 +24,17 @@ export class RoomComponent implements OnInit {
     this.name = event.target.value
   }
 
-  handleSubmit(event: any) {
-    console.log("Creating a room with name: " + this.name)
+  async handleSubmit(event: any) {
     event.preventDefault() 
+    console.log("Creating a room with name: " + this.name)
+    // TODO: Backend
+    // const roomResults: any = await API.graphql(graphqlOperation(createRoom, {name: this.name}))
+    // console.log(roomResults)
+    // frontend only
+    let room = {
+      id: uuid(),
+      name: this.name,
+      code: "test code" // nanoId
+    }
   }
-
 }
