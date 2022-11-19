1134a5ec825168c4962d2484e1806020bb520a1a
mjaydenkim <mjaydenkim@gmail.com>
2022-11-19 13:38:32 -0800

    Revert "backend updates"
    This reverts commit 0fbe2c27ccb966ee9dae4a516ee57bcb24fb6718.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,31 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
     "api": {
         "simplecalc": {
+            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "auth": {
-        "simplecalcbca74913bca74913": {
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "function": {
-        "simplecalcbca74913bca74913CustomMessage": {
-            "Name": "string",
-            "Arn": "string",
-            "LambdaExecutionRole": "string",
-            "Region": "string"
-        },
-        "simplecalcbca74913bca74913PostConfirmation": {
-            "Name": "string",
-            "Arn": "string",
-            "LambdaExecutionRole": "string",
-            "Region": "string"
         }
     }
 }
