940dc7fc8f239a7e83ad45cef15fbabed239b700
mjaydenkim <mjaydenkim@gmail.com>
2022-11-22 15:21:49 -0800

    Updated backend

--- amplify/backend/function/simplecalcbca74913bca74913PostConfirmation/src/index.js
+++ amplify/backend/function/simplecalcbca74913bca74913PostConfirmation/src/index.js
@@ -22,7 +22,43 @@ const modules = moduleNames.map(name => require(`./${name}`));
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  * 
  */
+
+ var aws = require('aws-sdk');
+ var ddb = new aws.DynamoDB();
+
 exports.handler = async (event, context) => {
+
+  let date = new Date();
+
+  if (event.request.userAttributes.sub) {
+
+    let params = {
+        Item: {
+            'id': {S: event.request.userAttributes.sub},
+            '__typename': {S: 'Teacher'},
+            'email': {S: event.request.userAttributes.email},
+            'createdAt': {S: date.toISOString()},
+            'updatedAt': {S: date.toISOString()},
+        },
+        TableName: process.env.USERTABLE_NAME
+    };
+
+    // Call DynamoDB
+    try {
+        await ddb.putItem(params).promise()
+        console.log("Success");
+    } catch (err) {
+        console.log("Error", err);
+    }
+
+    console.log("Success: Everything executed correctly");
+    context.done(null, event);
+
+  } else {
+    // Nothing to do, the user's email ID is unknown
+    console.log("Error: Nothing was written to DynamoDB");
+    context.done(null, event);
+  }
   /**
    * Instead of naively iterating over all handlers, run them concurrently with
    * `await Promise.all(...)`. This would otherwise just be determined by the
