c008150067a0e0fa40076bdf42d68b2bf144c38b
mjaydenkim <mjaydenkim@gmail.com>
2023-02-22 10:22:22 -0800

    backend updates

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,31 +1,32 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "simplecalc": {
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "auth": {
-        "userPoolGroups": {
-            "TeacherGroupRole": "string",
-            "StudentGroupRole": "string"
-        },
-        "simplecalcbca74913bca74913": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "function": {
-        "simplecalcbca74913bca74913PostConfirmation": {
-            "Name": "string",
-            "Arn": "string",
-            "LambdaExecutionRole": "string",
-            "Region": "string"
-        }
+  "api": {
+    "simplecalc": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
     }
+  },
+  "auth": {
+    "simplecalcbca74913bca74913": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "StudentGroupRole": "string",
+      "TeacherGroupRole": "string"
+    }
+  },
+  "function": {
+    "simplecalcbca74913bca74913PostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
 }
