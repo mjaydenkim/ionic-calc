31214e1a723223cfc04f8b30487c5e02b4029bdd
mjaydenkim <mjaydenkim@gmail.com>
2023-04-11 10:44:34 -0700

    Create graph-display component, multi-function graphing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
     declarations: [AppComponent],
@@ -15,7 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
         IonicModule.forRoot(),
         AppRoutingModule,
         BrowserAnimationsModule,
-        // KatexModule
+        IonicStorageModule.forRoot(),
+        ComponentsModule
         ],
     providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
     bootstrap: [AppComponent],
