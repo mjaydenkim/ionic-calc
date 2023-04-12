e60f480d8196aa23f6088b4e24601e5b761b8ad0
mjaydenkim <mjaydenkim@gmail.com>
2023-04-12 15:54:25 -0700

    Reverting upstream merge from last year
    (Added a lot of Amplify files into the offline branch)

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "simplecalc": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
