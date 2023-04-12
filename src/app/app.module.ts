501be9fb07ee1e019c9abf3e7a77a5df57e21dd5
mjaydenkim <mjaydenkim@gmail.com>
2023-04-12 15:46:16 -0700

    Code review -- minor cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { KatexModule } from 'ng-katex'
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
-import { FormsModule } from '@angular/forms';
 import { ComponentsModule } from './components/components.module';
 
 @NgModule({
--- src/app/utilities/process.ts
+++ src/app/utilities/process.ts
@@ -6,6 +6,5 @@ export default function process(expr: string) {
     for (let i = 0; i < replaceList.length; i++) {
         expr = expr.replace(replaceList[i][0], replaceList[i][1])
     }
-    console.log(expr)
     return expr
 }
